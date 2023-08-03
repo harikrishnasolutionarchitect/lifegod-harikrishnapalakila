26743a86d75109c24e018c5411ddee48e87706cb
harikrishna_P <harikrishnapalakila@gmail.com>
2023-08-03 06:27:53 +0530

    Base shell scripting

---
+++ for-loop.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo ""
+
+Usage() {
+ echo ""
+ echo "INFO - Creating Function and need to work"
+ echo ""
+ echo "INFO - Usage: Need to use loop"
+ echo ""
+}
+
+Usage
+for a in 1 2 3 4 5
+do 
+   echo "Current Workig Directory is "
+   echo ""
+   pwd
+   echo "" 
+   ls -lrt
+   echo "${a}"
+done 
